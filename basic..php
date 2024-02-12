9b4b948529bac4cc2c0e00b67588c68286d5d65c
skm962 <159834019+skm962@users.noreply.github.com>
2024-02-12 16:43:55 -0500

    Add files via upload
    url

---
+++ basic..php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> <!-- Defines the document type as HTML -->
+<html lang="en"> <!-- Begins the HTML document and sets the language to English -->
+<head>
+    <meta charset="UTF-8"> <!-- Sets the character encoding to UTF-8 -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Sets the viewport for responsive design -->
+    <title>PHP - Hello World! </title> <!-- Assigns a title to the document -->
+</head>
+<body>
+<?php
+echo 'hello kalyan'; // Outputs "hello kalyan" using PHP
+?>
+<p><a href="index.php">return</a></p> <!-- Adds a link to return to the index.php page -->
+</body>
+</html>
---
+++ result.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+<h3>Hi There, <?php echo $_GET["firstName"]; ?> <?php echo $_GET["lastName"]; ?> from <?php echo $_GET["city"]; ?></h3>
+<a href="index.php">return</a>
+
+</body>
+</html>
